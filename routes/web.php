1739a9548cf0d87cbf96d4a72918e8d806a10f04
Hanan Ammar M <amarmaulana783@gmail.com>
2022-09-05 21:43:18 +0700

    add slicing detail movie

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ Route::prefix('prototype')->name('prototype.')->group(function () {
         return Inertia::render('Prototype/SubscriptionPlan');
     })->name('subscriptionPlan');
 
+    Route::get('/movie/{slug}', function () {
+        return Inertia::render('Prototype/Movie/Show');
+    })->name('movie.show');
+
 });
 
 
