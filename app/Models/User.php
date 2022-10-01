ce5be3c03d49c45524ff32af7fdca7f84d4030e7
Hanan Ammar M <amarmaulana783@gmail.com>
2022-10-02 01:24:28 +0700

    integrate sidebar & subsciprtionplandetail

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use \Illuminate\Database\Eloquent\Relations\HasOne;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -42,4 +45,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getIsActiveAttribute()
+    {
+        if (!$this->LastActiveUserSubscription()) {
+            return false;
+        }
+        $dateNow = Carbon::now();
+        $dateExpired = Carbon::create($this->LastActiveUserSubscription()->expired_date)->addDays(30);
+        return $dateNow->lessThanOrEqualTo($dateExpired);
+    }
+
+    /**
+     * Get the LastActiveUserSubscription associated with the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function LastActiveUserSubscription(): HasOne
+    {
+        return $this->hasOne(UserSubscription::class)->wherePaymentStatus('success')->latest();
+    }
 }
--- resources/js/Layouts/Authenticated/Index.jsx
+++ resources/js/Layouts/Authenticated/Index.jsx
@@ -6,7 +6,7 @@ export default function Authenticated({ auth, children }) {
         <>
             <div className="mx-auto max-w-screen hidden lg:block">
                 {/* START: Sidebar */}
-                <Sidebar />
+                <Sidebar auth={auth} />
                 {/* END: Sidebar */}
                 {/* START: Content */}
                 <div className="ml-[300px] px-[50px]">
